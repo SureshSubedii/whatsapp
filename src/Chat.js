1fc66febb042bed6b4de47b9fb65b730c5f3dac0
SureshSubedii <sureshsubedi485@gmail.com>
2023-04-23 20:06:13 +0545

    Frontend Complete

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Chat.css'
 import { Avatar, IconButton } from '@mui/material'
-import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material'
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@mui/icons-material'
 
 function Chat() {
   return (
@@ -29,13 +29,48 @@ function Chat() {
         </div>
       </div>
       <div className="chat_body">
-        <p>
+        <p className="chat_message">
           <span className="chat_name">Loop</span>
           This is the message
           <span className="chat_timestamp">{new Date().toLocaleString()}</span>
-
-
         </p>
+        <p className="chat_message">
+          <span className="chat_name">Loop</span>
+          This is the message
+          <span className="chat_timestamp">{new Date().toLocaleString()}</span>
+        </p>
+       
+        <p className="chat_receiver chat_message">
+          <span className="chat_name">Loop</span>
+          This is the message
+          <span className="chat_timestamp">{new Date().toLocaleString()}</span>
+        </p>
+        <p className="chat_receiver chat_message">
+          <span className="chat_name">Loop</span>
+          This is the message
+          <span className="chat_timestamp">{new Date().toLocaleString()}</span>
+        </p>
+        <p className="chat_receiver chat_message">
+          <span className="chat_name">Loop</span>
+          This is the message
+          <span className="chat_timestamp">{new Date().toLocaleString()}</span>
+        </p>
+        <p className="chat_receiver chat_message">
+          <span className="chat_name">Loop</span>
+          This is the message
+          <span className="chat_timestamp">{new Date().toLocaleString()}</span>
+        </p>
+
+      </div>
+      <div className="chat_footer">
+        <InsertEmoticon/>
+        <form>
+          <input placeholder='Type a message'/>
+          <button type='submit'> Send a message</button>
+
+
+        </form>
+        <Mic/>
       </div>
     </div>
   )
